5b30a3153366689bef1c2a4a91ad2bf9f646ccba
Arturo <arturopll55@gmail.com>
2025-06-23 18:46:59 +0000

    Update demodebiasing

---
+++ notebooks/RunDemoDebiasing.py
@@ -0,0 +1,49 @@
+import papermill as pm
+
+MODELS = [
+    'bert-base-uncased',
+    'deepseek-ai/deepseek-llm-7b-base',
+    'huggyllama/llama-7b'
+]
+
+TASKS = [
+    "cola",
+    "sst2",
+    "mrpc",
+    "stsb",
+    "qqp",
+    "mnli",
+    "qnli",
+    "rte",
+    "wnli"
+]
+
+DEBIAS_METHODS = [
+    "none",
+    "cda",
+    "blind",
+    "embedding",
+    "ear",
+    "adele",
+    "selective",
+    "eat",
+    "diff"
+]
+
+param_grid = [
+    {
+        "MODEL_NAME": model_name,
+        "TASK": task,
+        "DEBIAS": debias
+    } for model_name in MODELS for task in TASKS for debias in DEBIAS_METHODS
+]
+
+for i, params in enumerate(param_grid, 1):
+    try:
+        pm.execute_notebook(
+            "notebooks/DemoDebiasing.ipynb",
+            "notebooks/tmp.ipynb",    # notebook de salida
+            parameters=params               
+        )
+    except Exception as e:
+        print(e)
