245d100716101de27dac1447b27da6408b60720d
Arturo <arturopll55@gmail.com>
2025-07-10 09:10:16 +0000

    Update adapters

--- FairLangProc/algorithms/inprocessors/adapter.py
+++ FairLangProc/algorithms/inprocessors/adapter.py
@@ -1,40 +1,61 @@
+# Standard imports
+from typing import Union
+
+# Pytorch
 import torch
 import torch.nn as nn
+
+# Adapters
 import adapters
+
 
 class DebiasAdapter(nn.Module):
     """
-    Initialize a model and add an adapter.
+    Implements ADELE debiasing based on bottleneck adapter.
     
     Args:
-        model (nn.Module):  Pretrained model
-        config (str):       Name of the adapter's configuration 
+        model (nn.Module):                  Pretrained model (e.g., BERT, GPT-2)
+        adapter_name (str):                 Tensor with ids of text with demographic information of group A
+        adapter_config (Union[str, dict]):  Name or dictionary of the desired configuration for the adapter (bottleneck by default)
     """
-
     def __init__(
-            self,
-            model: nn.Module,
-            config = 'lora'
-            ):
+        self,
+        model: nn.Module,
+        adapter_name: str = "debias_adapter",
+        adapter_config: Union[str, dict] = "seq_bn",
+    ):
+        
         super().__init__()
-        
+        self.adapter_name = adapter_name
+        adapters.init(model)
         self.model = model
-        # Add an adapter
-        adapters.init(self.model)
-        
-        # Freeze the original model parameters to only train the adapter:
-        self.model.add_adapter('debias_adapter', config = config)
-        self.model.train_adapter('debias_adapter')
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None, labels=None):
-        """
-        forward pass
-        """
-        # Forward pass is as usual; the adapters will be automatically applied
-        outputs = self.model.forward(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            labels=labels
-        )
-        return outputs
+        # Verify support
+        if not hasattr(self.model, "add_adapter"):
+            raise ValueError("Model does not support adapters.")
+
+        # Load adapter config
+        if isinstance(adapter_config, str):
+            config = adapters.AdapterConfig.load(adapter_config)
+        elif isinstance(adapter_config, dict):
+            config = adapters.AdapterConfig(**adapter_config)
+        else:
+            config = adapter_config
+
+        # Add adapter and set it up
+        self.model.add_adapter(adapter_name, config=config)
+        self.model.set_active_adapters(adapter_name)
+        self.model.train_adapter(self.adapter_name)
+
+    def forward(self, **kwargs):
+        return self.model(**kwargs)
+    
+    def get_model(self):
+        return self.model
+
+    def save_adapter(self, save_path: str):
+        self.model.save_adapter(save_path, self.adapter_name)
+
+    def load_adapter(self, path: str):
+        self.model.load_adapter(path, load_as=self.adapter_name)
+        self.model.set_active_adapters(self.adapter_name)
